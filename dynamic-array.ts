59bf1c8111d9e1cd0bb985aa83ad3159959e6f7f
ramtin <ramtinravare1380@gmail.com>
2023-07-19 12:47:03 +0330

    adding dynamic array interface and some changes

--- dynamic-array.ts
+++ dynamic-array.ts
@@ -1,10 +1,18 @@
-class DynamicArray<T> {
-  public array = new Array<T>();
-  private count = 0;
+interface IDynamicArray<T> {
+  array: T[];
+  insert(element: T): void;
+  removeAt(index: number): void;
+  indexOf(element: T): number;
+  print(): void;
+}
+
+class DynamicArray<T> implements IDynamicArray<T> {
+  public array: T[] = new Array<T>();
+  private _count: number = 0;
 
   constructor(length: number, ...items: T[]) {
     this.array.length = length;
-    this.count = items.length;
+    this._count = items.length;
     if (items.length <= length) {
       for (let i: number = 0; i < items.length; i++) {
         this.array[i] = items[i];
@@ -18,35 +26,35 @@ class DynamicArray<T> {
 
   // runtime complexity = O(n)
   public insert(element: T): void {
-    if (this.array.length === this.count) {
+    if (this.array.length === this._count) {
       let newArray: T[] = new Array(this.array.length * 2);
-      for (let i: number = 0; i < this.count; i++) {
+      for (let i: number = 0; i < this._count; i++) {
         newArray[i] = this.array[i];
       }
       this.array = newArray;
     }
-    this.array[this.count] = element;
-    this.count++;
+    this.array[this._count] = element;
+    this._count++;
   }
   // runtime complexity = O(n)
   public removeAt(index: number): void {
-    if (index >= this.count || index < 0)
-      throw new Error("the index should be between 0 to " + (this.count - 1));
-    for (let i: number = index; i < this.count; i++) {
+    if (index >= this._count || index < 0)
+      throw new Error("the index should be between 0 to " + (this._count - 1));
+    for (let i: number = index; i < this._count; i++) {
       this.array[i] = this.array[i + 1];
     }
-    this.count--;
+    this._count--;
   }
   // runtime complexity = O(n)
   public indexOf(element: T): number {
-    for (let i: number = 0; i < this.count; i++) {
+    for (let i: number = 0; i < this._count; i++) {
       if (this.array[i] === element) return i;
     }
     return -1;
   }
   // runtime complexity = O(n)
   public print(): void {
-    for (let i: number = 0; i < this.count; i++) {
+    for (let i: number = 0; i < this._count; i++) {
       console.log(this.array[i]);
     }
   }
